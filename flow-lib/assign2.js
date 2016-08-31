c3e60db1ae1d57703d7347ff5fd7c9f17d855400
justinwoo <moomoowoo@gmail.com>
2016-08-31 07:40:23 +0200

    declare function is how you add overloading signatures in flow huh

--- flow-lib/assign2.js
+++ flow-lib/assign2.js
@@ -10,11 +10,11 @@ declare module './assign2' {
 
     // it seems that only the last definition is being selected when trying to
     // type this local module variadic function
-    assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object, G: Object>(a: A, b: B, c: C, d: D, e: E, f: F, g: G): A & B & C & D & E & F & G;
-    assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object>(a: A, b: B, c: C, d: D, e: E, f: F): A & B & C & D & E & F;
-    assign2<A: Object, B: Object, C: Object, D: Object, E: Object>(a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
-    assign2<A: Object, B: Object, C: Object, D: Object>(a: A, b: B, c: C, d: D): A & B & C & D;
-    assign2<A: Object, B: Object, C: Object>(a: A, b: B, c: C): A & B & C;
+    // assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object, G: Object>(a: A, b: B, c: C, d: D, e: E, f: F, g: G): A & B & C & D & E & F & G;
+    // assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object>(a: A, b: B, c: C, d: D, e: E, f: F): A & B & C & D & E & F;
+    // assign2<A: Object, B: Object, C: Object, D: Object, E: Object>(a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
+    // assign2<A: Object, B: Object, C: Object, D: Object>(a: A, b: B, c: C, d: D): A & B & C & D;
+    // assign2<A: Object, B: Object, C: Object>(a: A, b: B, c: C): A & B & C;
     // assign2<A: Object, B: Object>(a: A, b: B): A & B;
     // assign2<A: Object>(a: A): A;
   }
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* @flow */
 
-import {assign2} from './assign2';
+// import {assign2} from './assign2';
+
+// declare function assign2<A: Object>(a: A): A;
+// declare function assign2<A: Object, B: Object>(a: A, b: B): A & B;
+// declare function assign2<A: Object, B: Object, C: Object>(a: A, b: B, c: C): A & B & C;
+// declare function assign2<A: Object, B: Object, C: Object, D: Object>(a: A, b: B, c: C, d: D): A & B & C & D;
+// declare function assign2<A: Object, B: Object, C: Object, D: Object, E: Object>(a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
+// declare function assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object>(a: A, b: B, c: C, d: D, e: E, f: F): A & B & C & D & E & F;
+
+declare function assign2<A: Object, B: Object, C: Object, D: Object, E: Object, F: Object>(a: A, b: B, c: C, d: D, e: E, f: F): A & B & C & D & E & F;
+declare function assign2<A: Object, B: Object, C: Object, D: Object, E: Object>(a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
+declare function assign2<A: Object, B: Object, C: Object, D: Object>(a: A, b: B, c: C, d: D): A & B & C & D;
+declare function assign2<A: Object, B: Object, C: Object>(a: A, b: B, c: C): A & B & C;
+declare function assign2<A: Object, B: Object>(a: A, b: B): A & B;
+declare function assign2<A: Object>(a: A): A;
+function assign2(...objects) {
+  var init = objects[0];
+  for (var i = 1; i < objects.length; i++) {
+    var obj = objects[i];
+    for (var k in obj) {
+      if (obj.hasOwnProperty(k)) {
+        init[k] = obj[k];
+      }
+    }
+  }
+  return init;
+}
 
 type A = {a: boolean, b: boolean};
 type B = A & {c: boolean};
